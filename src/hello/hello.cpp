914774296c97fe69fe015dc0fd8b1cf8df06d391
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-06-28 21:19:15 -0500

    Add pybind11 binding and CMake build process
    The pybind11 binding for the C++ library is added under src/pyhello.
    The 'py' prefix is common for Python wrappers around existing libraries.
    The sources are placed in the similarly named directory under src to
    segregate them, but also because this directory will be the eventual
    location of the Python package.
    
    See the CMakeLists.txt for details on what was changed with the build
    system.
    
    Building the pybind11 module is the same as the rest of the library:
    > cmake -S . -B build
    > cmake --build build
    
    The _pyhello extension module will be located in the build/ directory.
    You can test it manually.
    > python -i
    >> import build._pyhello as hello
    >> hello.hello()
    >> "Hello, World!"

---
+++ src/hello/hello.cpp
@@ -0,0 +1,11 @@
+#include <pybind11/pybind11.h>
+#include "hello/hello.hpp"
+
+namespace py = pybind11;
+
+
+PYBIND11_MODULE(_hello, m)
+{
+    m.def("hello", &hello::hello, "Says hello", py::arg("your_name") = "World");
+    m.def("return_answer_to_life", &hello::return_answer_to_life, "Come back in 7.5 million years to learn the answer to everything");
+}
