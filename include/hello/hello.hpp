0e31e5309641b6c834381040b9eee8ab63623230
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-07-02 18:33:43 -0500

    Add Doxygen documentation
    Documentation was added to the header only. `doxygen -g` was used to
    generate a default Doxyfile, which was updated with information
    pertaining to this project.
    
    One can manually generate the documentation with
    > cd docs
    > doxygen Doxyfile

--- include/hello/hello.hpp
+++ include/hello/hello.hpp
@@ -4,11 +4,23 @@
 #include <string>
 
 
+/** Hello
+ *
+ * Contains example functions.
+ */
 namespace hello {
 
-
+/** Says hello
+ *
+ * @param name Says hello to the given name. If no name is given, it says hello to the world.
+ * @returns The greeting as a string.
+ */
 std::string hello(char const * name = "World");
 
+/** Answers an important question
+ *
+ * @returns Answer to the Ultimate Question of Life, the Universe, and Everything.
+ */
 int return_answer_to_life();
 
 }
