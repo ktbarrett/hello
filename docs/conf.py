eb26f23c4175827d9663989f37e3714f2fc1ec3a
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-07-02 19:37:53 -0500

    Setup Sphinx documention
    Sphinx is a more powerful documentation tool than Doxygen, and with the
    help of the Breathe extension we can use the existing doxygen in-source
    documentation in Sphinx. In addition, we can document the Python
    wrapper library as well.
    
    To generate the sphinx documentation first generate the doygen
    documentation
    > cd docs
    > doxygen Doxyfile
    
    Then you can build the sphinx documentation
    > sphinx -b . out

---
+++ docs/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'hello'
+copyright = '2020, Kaleb Barrett'
+author = 'Kaleb Barrett'
+
+# The full version, including alpha/beta/rc tags
+release = '0.0.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx_rtd_theme",
+    "breathe"
+]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+
+# -- Breathe extension -------------------------------------------------------
+
+breathe_projects = {'hello': "_doxygen_build/xml"}
+breathe_default_project = "hello"
